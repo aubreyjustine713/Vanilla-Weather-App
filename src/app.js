067588d264063dae004ab82c3b6451730915e599
aubreyjustine713 <aubreyjustinejamison@gmail.com>
2023-04-16 19:09:47 -0400

    Updated date & time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.temperature.current
@@ -10,6 +29,8 @@ function displayTemperature(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let dateElement = document.querySelector("#date-time");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 let apiKey = "9t00ecb4eo3189a04cbb6f6f58a37ac9";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query={Newmarket}&key=${apiKey}&units=metric`;
