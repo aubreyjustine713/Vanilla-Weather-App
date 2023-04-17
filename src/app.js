f24633d14044bf1281281fd86928ef600bd330f6
aubreyjustine713 <aubreyjustinejamison@gmail.com>
2023-04-17 07:55:09 -0400

    Added search engine

--- src/app.js
+++ src/app.js
@@ -38,8 +38,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.condition.description);
 }
-let apiKey = "9t00ecb4eo3189a04cbb6f6f58a37ac9";
-let city = "Newmarket";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "9t00ecb4eo3189a04cbb6f6f58a37ac9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
